c3b9aba73ff96c8526a7a4b0adc13b90f1d157c4
nguyenhuuca <nguyenhuuca@gmail.com>
2015-11-22 22:48:01 +0700

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,38 @@
+echo 'bower version'
+bower --version
+echo 'get libary by bower'
+bower install
+
 cd bower_components/angular
-for f in *.js
-do
-	cp $f ../../assets/angular/
-	echo 'copyinng:' $f 
-done
+echo 'starting copy angular lib'
+#for f in *.js
+#do
+#	cp $f ../../assets/angular/
+#	echo 'copying:' $f 
+#done
+mkdir -p ../../assets/angular && cp -r *.js ../../assets/angular
+echo 'copy angular successfully'
+
+cd ../../bower_components/bootstrap/dist
+echo 'starting copy bootstrap'
+mkdir -p ../../../assets/bootstrap/ && cp -r * ../../../assets/bootstrap/
+echo 'copy boostap successfully'
+
+
+echo 'starting copy font-awesome'
+cd ../../../bower_components/font-awesome/css
+mkdir -p ../../../assets/font-awesome/css && cp -r * ../../../assets/font-awesome/css
+cd ../fonts
+mkdir -p ../../../assets/font-awesome/fonts && cp -r * ../../../assets/font-awesome/fonts
+echo 'copy font-awesome successfully'
+
+echo 'starting copy jquery'
+cd ../../../bower_components/jquery/dist
+mkdir -p ../../../assets/jquery/ && cp -r * ../../../assets/jquery/
+echo 'copy jquery successfully'
+
+echo 'complete install'
+
+
+
+
